77055a067cd661613dc89083fad864a18955d2c0
eshta <eshtadevatha06@gmail.com>
2025-03-02 22:04:57 +0530

    Updated feature/fixed bug

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: '0.0.0.0', // Allow external access
-    port: 3000 // Set a specific port
+    port: 3000, // Set a specific port
+    allowedHosts: ['compiler-design-project.onrender.com'], // Allow this host
   }
 });
